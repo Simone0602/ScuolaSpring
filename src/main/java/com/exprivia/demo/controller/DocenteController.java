3872e75c9d0693584c7a4834177a4d4db7429cc2
LAPTOP-J69OJ06J\Simone <simonedamato8@gmail.com>
2023-01-24 11:55:34 +0100

    no message

--- src/main/java/com/exprivia/demo/controller/DocenteController.java
+++ src/main/java/com/exprivia/demo/controller/DocenteController.java
@@ -1,8 +1,20 @@
 package com.exprivia.demo.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exprivia.demo.dto.StudenteDto;
+import com.exprivia.demo.model.Studente;
 import com.exprivia.demo.service.DocenteService;
 
 @RestController
@@ -15,6 +27,28 @@ public class DocenteController {
 		this.dservice = dservice;
 	}
 	
+	@GetMapping(path = "/dget")
+	public ResponseEntity<List<StudenteDto>> getAllStudent() {
+		List<StudenteDto> studenti = service.findAllStudent();
+		return new ResponseEntity<>(studenti, HttpStatus.OK);
+	}
 	
+	@PostMapping(path = "/dadd")
+	public ResponseEntity<Studente> addStudent(@RequestBody StudenteDto studenteDto) {
+		Studente studente = service.addStudent(studenteDto);
+		return new ResponseEntity<>(studente, HttpStatus.CREATED);
+	}
+
+	@PutMapping(path = "/dmod")
+	public ResponseEntity<Studente> modStudent(@RequestBody StudenteDto studenteDto) {
+		Studente studente = service.modStudent(studenteDto);
+		return new ResponseEntity<>(studente, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(path = "/ddel/{id}")
+	public ResponseEntity<?> delStudent(@PathVariable("id") long id) {
+		service.delStudent(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
