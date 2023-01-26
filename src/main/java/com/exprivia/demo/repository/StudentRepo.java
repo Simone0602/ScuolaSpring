cc9efc289d5c146228ecb1810ed3b94432f10675
LAPTOP-J69OJ06J\Simone <simonedamato8@gmail.com>
2023-01-26 09:46:49 +0100

    ok

--- src/main/java/com/exprivia/demo/repository/StudentRepo.java
+++ src/main/java/com/exprivia/demo/repository/StudentRepo.java
@@ -1,19 +1,19 @@
 package com.exprivia.demo.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.exprivia.demo.model.Studente;
 
 public interface StudentRepo extends JpaRepository <Studente, Long>{
 
 	public Optional<Studente> findStudentByUserCode(String userCode);
-	
-	public Optional<Studente> findStudentByMail(String mail);
 
 	public boolean existsByUserCode(String userCode);
 
-	public boolean existsByMail(String mail);
+	@Query(value = "SELECT * FROM studenti WHERE email = :mail and password = :pas and user_code = :user_code", nativeQuery = true)
+	public Studente findStudent(@Param("mail") String mail, @Param("pas") String pas, @Param("user_code") String userCode);
 }
